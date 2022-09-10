379bdd5838b5f3af2f492f2bda56f80b6c565548
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-10 10:57:33 -0500

    probando endpoint con user

--- src/controllers/users.ctrl.js
+++ src/controllers/users.ctrl.js
@@ -1,24 +1,47 @@
-
 const User = require("../models/user");
 
 const getUsuarios = async (req, res) => {
-  const usuarios = await User.find()
+  const usuarios = await User.find();
   return res.json({
     ok: true,
     msg: "Se obtuvieron los usuarios ",
     data: usuarios,
   });
 };
-// const postUsuarios = (req, res) => {
-//   const { name, id, pd } = req.body;
-//   const nuevaPersona = {};
+const crearUsuario = async (req, res) => {
+  const { email, username, password } = req.body;
+  const user = {
+    email,
+    username,
+    password,
+  };
+const newUser = await User.create(user)
+  return res.json({
+    msg: `Se creo un nuevo usuario llamado ${username} `,
+    data: newUser,
+  });
+};
 
-//   return res.json({
-//     msg: `Se creo un nuevo usuario llamado ${name}`,
-//     data: nuevaPersona,
-//   });
-// };
+const updateUser = (req, res) => {
+  const { idUser } = req.params;
+  const { user_name, password } = req.body;
+
+  const usuarioEncontrado = users.find((user) => {
+    return user.id === idUser;
+  });
+
+  usuarioEncontrado.user_name = user_name;
+  usuarioEncontrado.password = password;
+
+  return res.json({
+    ok: true,
+    msg: "Usuario actualizado",
+    data: usuarioEncontrado,
+  });
+};
+
+
 module.exports = {
   getUsuarios,
-  // postUsuarios,
+  crearUsuario,
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,15 +1,11 @@
 const { Router } = require("express");
-const { getUsuarios } = require("../controllers/users.ctrl");
+const { getUsuarios, crearUsuario , } = require("../controllers/users.ctrl");
 
 const router = Router();
 // router.get('/',getUsuarios)
 router.get ("/", getUsuarios);
 
-router.post("/", (req, res) => {
-  return res.json({
-    msg: "post",
-  });
-});
+router.post("/", crearUsuario);
 
 router.put("/", (req, res) => {
   return res.json({
